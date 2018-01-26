11aafbe796587885243edac7b6c63877dc228daf
ZC44 <2227781C@student.gla.ac.uk>
2018-01-26 14:12:39 +0000

    Chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from rango.models import *
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+         (None, {'fields': ['name']}),
+         ('Data', {'fields':['likes','views']}),
+    ]
+class PageAdmin(admin.ModelAdmin):
+    fieldsets = [
+         (None, {'fields': ['title', 'url', 'category']}),
+      ]
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
